64e5bb74bd5833b25e65c8d2483b114fb47b7327
Daniel Knauf <daniel.knaut@posteo.de>
2021-10-14 06:53:51 +0200

    0.1.2
    - increase version code

--- buildSrc/src/main/kotlin/BuildConfig.kt
+++ buildSrc/src/main/kotlin/BuildConfig.kt
@@ -1,14 +1,15 @@
 import org.gradle.api.JavaVersion
 
 object BuildConfig {
+
     object SdkVersion {
         const val compile = 30
         const val min = 21
         const val target = 30
     }
 
-    const val versionCode = 2
-    const val versionName = "0.1.1"
+    const val versionCode = 3
+    const val versionName = "0.1.2"
 
     val javaVersion = JavaVersion.VERSION_1_8
 }
